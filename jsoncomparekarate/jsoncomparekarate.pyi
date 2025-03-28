d803796f5e151da1635e3c752509ec288d813eb5
Vinit Patel <vinitp2004@yahoo.com>
2025-03-28 10:45:50 -0700

    Add files via upload

---
+++ jsoncomparekarate/jsoncomparekarate.pyi
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# author: Vin Pat  mail to: eatfrogfirst@outlook.com
+
+def compare(a, b, strict_json_check=False):
+    """this function returns whether a is matched with b
+    :param str or list or dict a: The first json string/json-like object to compare
+    :param str or list or dict b: The second one to be compared
+    :param bool strict_json_check: Whether to check the strict json format
+    :return bool: Whether two json string or json-like objects are equal. If not, print the differences
+    """
+    return True
